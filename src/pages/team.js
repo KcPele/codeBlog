a284f7252d524680279d026ef82734c1471651a7
kc emmanuel <fidekg123@gmail.com>
2020-09-09 22:16:04 +0100

    added image-icon

--- src/pages/team.js
+++ src/pages/team.js
@@ -5,11 +5,11 @@ import Layout from "../components/layout"
 
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const TeamPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Home page</h1>
+    <SEO title="Team" />
+    <h1>Our Team</h1>
     </Layout>
 )
 
-export default IndexPage
+export default TeamPage
