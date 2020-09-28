8ceb61f43d93d4a86dcad63b5d3dec70514135b3
kc emmanuel <fidekg123@gmail.com>
2020-09-28 21:54:55 +0100

    created all tags page

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,7 @@
 const { slugify } = require('./src/util/utilityFunction')
 const path = require('path')
 const authors = require('./src/util/authors')
+const _ = require('lodash')
 
 
 exports.onCreateNode = ({ node, actions}) => {
@@ -23,7 +24,12 @@ exports.onCreateNode = ({ node, actions}) => {
 
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions
-    const singlePostTemplate = path.resolve('src/templates/single-post.js')
+   const templates = {
+        singlePost: path.resolve('src/templates/single-post.js'),
+        tagsPage: path.resolve('src/templates/tags-pages.js')
+
+    }
+
 
     return graphql(`
        {
@@ -32,7 +38,7 @@ exports.createPages = ({ actions, graphql }) => {
                    node{
                        frontmatter{
                            author
-                           
+                           tags
                        }
                        fields{
                            slug
@@ -44,17 +50,42 @@ exports.createPages = ({ actions, graphql }) => {
     `).then(res => {
         if(res.errors) return Promise.reject(res.errors)
         const posts = res.data.allMarkdownRemark.edges
+        //create single post
         posts.forEach(({ node }) => {
             createPage({
                 path: node.fields.slug,
-                component: singlePostTemplate,
+                component: templates.singlePost,
                 context: {
                     //passing slug for template to use to get post
                     slug: node.fields.slug,
                     //find authors imageUrl from authors and pass it to the singlepost template
                     imageUrl: authors.find(x => x.name === node.frontmatter.author).imageUrl
-                }
+                },
             })
         });
+        //Get All Tags
+        let tags = []
+        _.each(posts, edge => {
+            if(_.get(edge, 'node.frontmatter.tags')){
+                tags = tags.concat(edge.node.frontmatter.tags)
+       
+            }
+            })
+            //['design', 'code', ....]
+            let tagPostCounts = {}
+            tags.forEach(tag => {
+                tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1;
+            })
+
+            tags = _.uniq(tags)
+     //create tags page
+            createPage({
+                path: '/tags',
+                component: templates.tagsPage,
+                context: {
+                    tags,
+                    tagPostCounts
+                }
+            })
     })
 }
